652a614e9c6a0191231f5c84c5a2b69e9b77e8ec
Barun Sarraf <barun.sarraf@cgi.com>
2019-10-13 20:48:47 +0530

    including docker file

---
+++ apiinterface/src/test/java/com/example/apiinterface/ApiinterfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.apiinterface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiinterfaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
